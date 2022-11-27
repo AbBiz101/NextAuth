3c6d77bbca94a16ad400c3d86295dc118eba5bfc
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-27 05:13:37 +0100

    Update user-profile.js

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,26 +1,26 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { getSession } from 'next-auth/client';
-import { useState, useEffect } from 'react';
+// import { getSession } from 'next-auth/client';
+// import { useState, useEffect } from 'react';
 
 function UserProfile() {
 	// Redirect away if NOT auth
-	const [loading, setLoading] = useState(true);
-	const [session, setSession] = useState();
+	// const [loading, setLoading] = useState(true);
+	// const [session, setSession] = useState();
 
-	useEffect(() => {
-		getSession().then((session) => {
-			if (!session) {
-				window.location.href = '/auth';
-			} else {
-				setLoading(false);
-			}
-		});
-	}, []);
+	// useEffect(() => {
+	// 	getSession().then((session) => {
+	// 		if (!session) {
+	// 			window.location.href = '/auth';
+	// 		} else {
+	// 			setLoading(false);
+	// 		}
+	// 	});
+	// }, []);
 
-	if (loading) {
-		return <h2 className={classes.profile}>Loading...</h2>;
-	}
+	// if (loading) {
+	// 	return <h2 className={classes.profile}>Loading...</h2>;
+	// }
 
 	return (
 		<section className={classes.profile}>
