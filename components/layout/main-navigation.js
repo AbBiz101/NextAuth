9410cb627e65207ed110c5b328b934b290b860d5
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-27 04:34:29 +0100

    Update main-navigation.js

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,30 +1,44 @@
 import Link from 'next/link';
-
+import { useSession, signOut } from 'next-auth/client';
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <Link href='/'>
-        <a>
-          <div className={classes.logo}>Next Auth</div>
-        </a>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	const [session, loading] = useSession();
+
+	function logOutHandler() {
+		signOut();
+	}
+
+	console.log(session, loading);
+
+	return (
+		<header className={classes.header}>
+			<Link href="/">
+				<a>
+					<div className={classes.logo}>Next Auth</div>
+				</a>
+			</Link>
+			<nav>
+				<ul>
+					{!session && !loading && (
+						<li>
+							<Link href="/auth">Login</Link>
+						</li>
+					)}
+					{session && (
+						<li>
+							<Link href="/profile">Profile</Link>
+						</li>
+					)}
+					{session && (
+						<li>
+							<button onClick={logOutHandler}>Logout</button>
+						</li>
+					)}
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default MainNavigation;
