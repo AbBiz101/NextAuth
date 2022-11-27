746ec245aefc94cf2e09a9cad5779a7cd2e6158d
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-27 04:34:25 +0100

    Update user-profile.js

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,15 +1,33 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
+import { getSession } from 'next-auth/client';
+import { useState, useEffect } from 'react';
 
 function UserProfile() {
-  // Redirect away if NOT auth
+	// Redirect away if NOT auth
+	const [loading, setLoading] = useState(true);
+	const [session, setSession] = useState();
 
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+	useEffect(() => {
+		getSession().then((session) => {
+			if (!session) {
+				window.location.href = '/auth';
+			} else {
+				setLoading(false);
+			}
+		});
+	}, []);
+
+	if (loading) {
+		return <h2 className={classes.profile}>Loading...</h2>;
+	}
+
+	return (
+		<section className={classes.profile}>
+			<h1>Your User Profile</h1>
+			<ProfileForm />
+		</section>
+	);
 }
 
 export default UserProfile;
