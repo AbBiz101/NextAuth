309028ca5023a8aa98c37940bf6325cc867e5576
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-27 03:21:26 +0100

    Update signup.js

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,8 +1,28 @@
 import { dbConnection } from '../../../utils/db';
+import hashPassword from '../../../utils/auth';
 
 export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		const { email, password } = req.body;
 
-	const client = await dbConnection();
-    const db = client.db();
-    
+		if (!email || !password) {
+			res.status(422).json({ message: 'invalid input' });
+		}
+		const hashedPassword = await hashPassword(password);
+		const client = await dbConnection();
+		const db = client.db();
+
+		const findUser = await db.collection('user').findOne({ email: email });
+		if (!findUser) {
+			const result = await db
+				.collection('user')
+				.insertOne({ email: email, password: hashedPassword });
+
+			res.status(201).json({ message: 'user created' });
+			db.close();
+		} else {
+			res.status(422).json({ message: 'user exists' });
+			db.close();
+		}
+	}
 }
