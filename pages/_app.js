b1a152c2d2f555ff9c56b27afea272cc1e8afdcd
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-28 19:36:18 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import { Provider } from 'next-auth/client';
+import Layout from '../components/layout/layout';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+export default function MyApp({ Component, pageProps }) {
+	return (
+		<Provider session={pageProps.session}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</Provider>
+	);
 }
-
-export default MyApp;
