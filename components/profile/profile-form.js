e9e847a378fac5ae9c580682d9fd640dd508b9e3
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-29 00:13:35 +0100

    Update profile-form.js

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,32 @@
 import classes from './profile-form.module.css';
+import { useRef } from 'react';
 
-export default function ProfileForm() {
+export default function ProfileForm(props) {
+	const newPasswordRef = useRef();
+	const oldPasswordRef = useRef();
+
+	function submitHandler(event) {
+		event.preventDefault();
+
+		const newPassword = newPasswordRef.current.value;
+		const oldPassword = oldPasswordRef.current.value;
+		props.onChangePassword({
+			newPassword: newPassword,
+			oldPassword: oldPassword,
+		});
+
+		
+	}
+
 	return (
-		<form className={classes.form}>
+		<form onSubmit={submitHandler} className={classes.form}>
 			<div className={classes.control}>
 				<label htmlFor="new-password">New Password</label>
-				<input type="password" id="new-password" />
+				<input ref={newPasswordRef} type="password" id="new-password" />
 			</div>
 			<div className={classes.control}>
 				<label htmlFor="old-password">Old Password</label>
-				<input type="password" id="old-password" />
+				<input ref={oldPasswordRef} type="password" id="old-password" />
 			</div>
 			<div className={classes.action}>
 				<button>Change Password</button>
