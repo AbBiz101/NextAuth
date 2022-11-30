20caf8fa2eda0395053c30efdd097f54550a7a85
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2022-11-30 18:57:07 +0100

    Update user-profile.js

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -2,26 +2,7 @@ import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 export default function UserProfile() {
-	// import { getSession } from 'next-auth/client';
-	// import { useState, useEffect } from 'react';
-	// Redirect away if NOT auth
-	// const [loading, setLoading] = useState(true);
-	// const [session, setSession] = useState();
-
-	// useEffect(() => {
-	// 	getSession().then((session) => {
-	// 		if (!session) {
-	// 			window.location.href = '/auth';
-	// 		} else {
-	// 			setLoading(false);
-	// 		}
-	// 	});
-	// }, []);
-
-	// if (loading) {
-	// 	return <h2 className={classes.profile}>Loading...</h2>;
-	// }
-
+	
 	async function changePasswordHandler(passwordData) {
 		console.log(passwordData);
 		const req = await fetch('/api/user/change-password', {
@@ -42,3 +23,23 @@ export default function UserProfile() {
 		</section>
 	);
 }
+
+		// import { getSession } from 'next-auth/client';
+		// import { useState, useEffect } from 'react';
+		// Redirect away if NOT auth
+		// const [loading, setLoading] = useState(true);
+		// const [session, setSession] = useState();
+	
+		// useEffect(() => {
+		// 	getSession().then((session) => {
+		// 		if (!session) {
+		// 			window.location.href = '/auth';
+		// 		} else {
+		// 			setLoading(false);
+		// 		}
+		// 	});
+		// }, []);
+	
+		// if (loading) {
+		// 	return <h2 className={classes.profile}>Loading...</h2>;
+		// }
